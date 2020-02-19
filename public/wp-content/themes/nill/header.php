52cfdf350cb75c99b73ff7f2ad2eeec827545fd2
Carlos Velazquez <carlosjvelazquez@gmail.com>
2020-02-19 15:25:59 -0500

    move the header and footer into separate files

---
+++ public/wp-content/themes/nill/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158618727-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-158618727-1');
+        </script>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
+        <meta name="description" content="Need a website? I can build you one. Click here for more details">
+        <meta name="keywords" content="web,frontend,backend,design,development,developer,html,css,html5,email,wordpress">
+        <meta name="author" content="Carlos Velazquez">
+        <title>Carlos.io - Web Developer</title>
+
+        <!--  FavIcon  -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <meta name="apple-mobile-web-app-title" content="Carlos.io">
+        <meta name="application-name" content="Carlos.io">
+        <meta name="msapplication-TileColor" content="#363636">
+        <meta name="theme-color" content="#363636">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:500,600,700" rel="stylesheet">
+
+        <!--  bootstrap Css  -->
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/public/assets/css/bootstrap.css">
+        <!--  pagepiling Css  -->
+       <!--  <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/public/assets/css/jquery.pagepiling.css" /> -->
+        <!--  LineIcon Css  -->
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/public/assets/css/LineIcons.css">
+        <!--  MagnificPopup Css  -->
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/public/assets/css/magnific-popup.css">
+        <!--  OwlCarousel Css  -->
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/public/assets/css/owl.carousel.min.css">
+        <!--  Custom Style CSS  -->
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/public/assets/css/style.css">
+
+    </head>
